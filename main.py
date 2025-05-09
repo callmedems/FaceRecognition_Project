9a487f32a422508458bb54136e763bca8c718abd
Ariana Villalobos <156480421+Peripera@users.noreply.github.com>
2025-05-09 03:31:36 -0600

    Adding haar cascade classifiers

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
 import cv2
 
-#Camera set up
-capture = cv2.VideoCapture(0) #Sets 0 at camera ID
-capture.set (3, 640) #set width
-capture.set (4, 480) #set height
+#import image
+
+video = cv2.VideoCapture(0) #Sets 0 at camera ID
+video.set (3, 640) #set width
+video.set (4, 480) #set height
+
+#Add haar cascade classifiers
+frontal_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+profile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_profileface.xml')
+
 
 #Loop that initiates the camera
 while True:
-    cap, img=capture.read()
+
+    capture, img=video.read()
+
+    #Detects faces on image
+    face_frontal=cv2.CascadeClassifier(frontal_cascade)
+
+    face_profile=cv2.CascadeClassifier(profile_cascade)
+
     cv2.imshow("Image", img)
+
     cv2.waitKey(1) #delay time
 
-
-
-#detect face in image
-
-
-
-#process and blur face in image
-
-#save image with blurred face
-
-
-#detect face in image
-
-#process and blur face in image
-
-#save image with blurred face
