6d04682106e9d888d62cdcb4102e65667dbfd597
Ariana Villalobos <156480421+Peripera@users.noreply.github.com>
2025-05-08 21:21:55 -0600

    Set up camera

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import cv2
-#import image
+
+#Camera set up
+capture = cv2.VideoCapture(0) #Sets 0 at camera ID
+capture.set (3, 640) #set width
+capture.set (4, 480) #set height
+
+#Loop that initiates the camera
+while True:
+    cap, img=capture.read()
+    cv2.imshow("Image", img)
+    cv2.waitKey(1) #delay time
+
+
+
+#detect face in image
+
+
+
+#process and blur face in image
+
+#save image with blurred face
+
 
 #detect face in image
 
